45edba8313629b7b32bb28b705cb004454b859b4
YomnaHamdi <ymnybdh668@gmail.com>
2024-09-08 23:01:01 +0300

    Update index.php to use the correct PORT

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
 echo "This is the index.php file running!";
+
+
+// الحصول على المنفذ من المتغير البيئي أو استخدام 9000 كمنفذ افتراضي
+$port = getenv('PORT') ?: 9000;
+$host = '0.0.0.0';
+
+// عرض رسالة في حالة بدء السيرفر
+echo "Server running on http://$host:$port\n";
+
+// تشغيل السيرفر باستخدام PHP's built-in server
+exec("php -S $host:$port -t public/");
+
