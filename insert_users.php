596a9ff3c15fea51ccbd6489ff455fe279ca1340
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-11 18:41:46 +0300

    Update insert_users.php

--- insert_users.php
+++ insert_users.php
@@ -7,10 +7,10 @@ $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 $User_Education = $_POST['User_Education'];
 $User_Experience = $_POST['User_Experience'];
-$User_Skills = $_POST['User_Skills'];
+$User_Skillls = $_POST['User_Skillls'];
 
-$sql_insert = "INSERT INTO users (first_name, last_name, email, password, User_Education, User_Experience, User_Skills) 
-VALUES ('$first_name', '$last_name', '$email', '$password', '$User_Education', '$User_Experience', '$User_Skills')";
+$sql_insert = "INSERT INTO users (first_name, last_name, email, password, User_Education, User_Experience, User_Skillls) 
+VALUES ('$first_name', '$last_name', '$email', '$password', '$User_Education', '$User_Experience', '$User_Skillls')";
 
 if (mysqli_query($con, $sql_insert)) {
     echo json_encode(["status" => "success", "message" => "User inserted successfully"]);
