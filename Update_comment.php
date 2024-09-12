48c00ac9257c2e6bb2960c6a3a9251cb846135bf
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-13 02:24:37 +0300

    Update Update_comment.php

--- Update_comment.php
+++ Update_comment.php
@@ -4,9 +4,9 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['Comment_id']) && isset($data['Comment'])) {
-    $stmt = $con->prepare("UPDATE comments SET Comment = ?, Uploaded_at = NOW() WHERE Comment_id = ?");
-    $stmt->bind_param("si", $data['Comment'], $data['Comment_id']);
+if (isset($data['comment_id']) && isset($data['comment'])) {
+    $stmt = $con->prepare("UPDATE comments SET comment = ?, created_at = NOW() WHERE comment_id = ?");
+    $stmt->bind_param("si", $data['comment'], $data['comment_id']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Comment updated successfully."));
