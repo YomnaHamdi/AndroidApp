38c4ebb93884173e58aeb875c0a2d215f0782ed1
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-12 18:03:09 +0300

    Update Get_UploadedWork.php

--- Get_UploadedWork.php
+++ Get_UploadedWork.php
@@ -2,15 +2,15 @@
 
 include_once 'db_connection.php';
 
-if (isset($_GET['work_id'])) {
-    $stmt = $con->prepare("SELECT * FROM uploadedwork WHERE Work_id = ?");
-    $stmt->bind_param("i", $_GET['work_id']);
+if (isset($_GET['Uploaded_id'])) {
+    $stmt = $con->prepare("SELECT * FROM UploadedWork WHERE Uploaded_id = ?");
+    $stmt->bind_param("i", $_GET['Uploaded_id']);
     $stmt->execute();
     $result = $stmt->get_result();
     echo json_encode($result->fetch_assoc());
 } else if (isset($_GET['user_id'])) {
-    $stmt = $con->prepare("SELECT * FROM uploadedwork WHERE User_id = ?");
-    $stmt->bind_param("i", $_GET['user_id']);
+    $stmt = $con->prepare("SELECT * FROM UploadedWork WHERE User_id = ?");
+    $stmt->bind_param("i", $_GET['User_id']);
     $stmt->execute();
     $result = $stmt->get_result();
     $works = array();
