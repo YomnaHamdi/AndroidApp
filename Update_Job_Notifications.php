0d16d5ef9a1243a8699676c4ddab1c7ab5d07c3e
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-13 18:54:13 +0300

    Update Update_Job_Notifications.php

--- Update_Job_Notifications.php
+++ Update_Job_Notifications.php
@@ -4,9 +4,9 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['notification_id']) && isset($data['notification_content'])) {
+if (isset($data['Notification_id']) && isset($data['Notification_content'])) {
     $stmt = $con->prepare("UPDATE job_notifications SET Notification_content = ?, Sent_at = NOW() WHERE Notification_id = ?");
-    $stmt->bind_param("si", $data['notification_content'], $data['notification_id']);
+    $stmt->bind_param("si", $data['Notification_content'], $data['Notification_id']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Notification updated successfully."));
