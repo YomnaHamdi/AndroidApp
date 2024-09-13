438d8cc825741fd4c654907b8c4aa2e0216defcb
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-13 18:14:53 +0300

    Update Update_CV.php

--- Update_CV.php
+++ Update_CV.php
@@ -6,14 +6,16 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 if (
     isset($data['cv_id']) &&
-    isset($data['languages']) &&
-    isset($data['skills']) &&
-    isset($data['work_experience']) &&
-    isset($data['education']) &&
-    isset($data['certifications'])
+    isset($data['User_id']) &&
+    isset($data['Languages']) &&
+    isset($data['Skills']) &&
+    isset($data['WorkExperience']) &&
+    isset($data['Education']) &&
+    isset($data['Certifications'])
+     
 ) {
-    $stmt = $con->prepare("UPDATE cv SET Languages = ?, Skills = ?, WorkExperience = ?, Education = ?, Certifications = ?, Updated_at = NOW() WHERE CV_id = ?");
-    $stmt->bind_param("sssssi", $data['languages'], $data['skills'], $data['work_experience'], $data['education'], $data['certifications'], $data['cv_id']);
+    $stmt = $con->prepare("UPDATE  Scurriculum_vitae SET User_id =?,Languages = ?, Skills = ?, WorkExperience = ?, Education = ?, Certifications = ?, Updated_at = NOW() WHERE cv_id = ?");
+    $stmt->bind_param("sssssi",$data['User_id'], $data['Languages'], $data['Skills'], $data['WorkExperience'], $data['Education'], $data['Certifications'], $data['cv_id']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "CV updated successfully."));
