95bc07ed897ebdd812b2a93d413b58a98a1c38e5
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-13 18:27:43 +0300

    Update Insert_job_application.php

--- Insert_job_application.php
+++ Insert_job_application.php
@@ -4,9 +4,9 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['user_id']) && isset($data['job_id']) && isset($data['status']) && isset($data['cv_id'])) {
-    $stmt = $con->prepare("INSERT INTO job_application (User_id, Job_id, Status, Applied_at, CV_id) VALUES (?, ?, ?, NOW(), ?)");
-    $stmt->bind_param("iisi", $data['user_id'], $data['job_id'], $data['status'], $data['cv_id']);
+if (isset($data['Application_id']) && isset($data['User_id']) && isset($data['job_id']) && isset($data['Status']) && isset($data['cv_id'])) {
+    $stmt = $con->prepare("INSERT INTO job_application (Application_id, User_id, job_id, Status, applied_at, cv_id) VALUES (?, ?, ?,?, NOW(), ?)");
+    $stmt->bind_param("iisi", $data['Application_id'],$data['User_id'], $data['job_id'], $data['Status'], $data['cv_id']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Job application created successfully."));
