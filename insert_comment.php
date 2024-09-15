2de71248c2c607c7000992e053c6597317f89589
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-16 00:28:54 +0300

    Update insert_comment.php

--- insert_comment.php
+++ insert_comment.php
@@ -4,19 +4,21 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['comment_id']) && isset($data['Uploaded_id']) && isset($data['User_id']) && isset($data['comment'])) {
+if (isset($data['Uploaded_id']) && isset($data['User_id']) && isset($data['comment'])) {
     $stmt = $con->prepare("INSERT INTO comments (Uploaded_id, User_id, comment, created_at) VALUES (?, ?, ?, NOW())");
-    $stmt->bind_param("iis",$data['comment_id'], $data['Uploaded_id'], $data['User_id'], $data['comment']);
+    $stmt->bind_param("iis", $data['Uploaded_id'], $data['User_id'], $data['comment']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Comment added successfully."));
     } else {
         echo json_encode(array("message" => "Error: " . $stmt->error));
     }
+
+    $stmt->close(); 
 } else {
     echo json_encode(array("message" => "Invalid input"));
 }
 
-mysqli_close($con);
+$con->close(); 
 
 ?>
