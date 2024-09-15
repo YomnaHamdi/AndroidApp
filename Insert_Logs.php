911242d9f3d9915d5c3958abe468bf4990c901ee
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-16 00:31:52 +0300

    Update Insert_Logs.php

--- Insert_Logs.php
+++ Insert_Logs.php
@@ -4,9 +4,9 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['Log_id']) &&isset($data['User_id']) && isset($data['Action'])) {
-    $stmt = $con->prepare("INSERT INTO logs (Log_id,User_id, Timestamp, Action) VALUES (?, NOW(), ?)");
-    $stmt->bind_param("is",$data['Log_id'], $data['User_id'], $data['Action']);
+if (isset($data['User_id']) && isset($data['Action'])) {
+    $stmt = $con->prepare("INSERT INTO logs (User_id, Timestamp, Action) VALUES (?, NOW(), ?)");
+    $stmt->bind_param("is", $data['User_id'], $data['Action']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Log added successfully."));
