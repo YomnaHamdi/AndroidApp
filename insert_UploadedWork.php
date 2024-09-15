fc3cd1c0abc1cb21b200aa977a84f3295203c5c7
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-16 00:19:46 +0300

    Update insert_UploadedWork.php

--- insert_UploadedWork.php
+++ insert_UploadedWork.php
@@ -4,9 +4,9 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['User_id']) && isset($data['Uploaded_id']) && isset($data['Title']) && isset($data['Description']) && isset($data['File'])) {
-    $stmt = $con->prepare("INSERT INTO UploadedWork (User_id, Uploaded_id, Title, Description, File, Uploaded_at) VALUES (?, ?, ?, ?, ?, NOW())");
-    $stmt->bind_param("iisss", $data['User_id'], $data['Uploaded_id'], $data['Title'], $data['Description'], $data['File']);
+if (isset($data['User_id']) && isset($data['Title']) && isset($data['Description']) && isset($data['File'])) {
+    $stmt = $con->prepare("INSERT INTO UploadedWork (User_id, Title, Description, File, Uploaded_at) VALUES (?, ?, ?, ?, NOW())");
+    $stmt->bind_param("isss", $data['User_id'],  $data['Title'], $data['Description'], $data['File']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Work uploaded successfully."));
