e9fb1bbdd0bb1ac3b74d908f97185c0de2f116ce
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-16 00:30:08 +0300

    Update Insert_Job_Notifications.php

--- Insert_Job_Notifications.php
+++ Insert_Job_Notifications.php
@@ -4,10 +4,10 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['Notification_id']) &&isset($data['User_id']) && isset($data['Job_id']) && isset($data['Notification_content'])) {
+if (isset($data['User_id']) && isset($data['Job_id']) && isset($data['Notification_content'])) {
     
-    $stmt = $con->prepare("INSERT INTO job_notifications (Notification_id,User_id, Job_id, Notification_content, Is_read, Sent_at) VALUES (?, ?, ?,?, 0, NOW())");
-    $stmt->bind_param("iis",$data['Notification_id'], $data['User_id'], $data['Job_id'], $data['Notification_content']);
+    $stmt = $con->prepare("INSERT INTO job_notifications (User_id, Job_id, Notification_content, Is_read, Sent_at) VALUES ( ?, ?,?, 0, NOW())");
+    $stmt->bind_param("iis", $data['User_id'], $data['Job_id'], $data['Notification_content']);
     
     
     if ($stmt->execute()) {
