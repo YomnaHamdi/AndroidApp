53c5e99c3ce4a8bfcda8e74fdabe1545d0c70559
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-16 16:06:56 +0300

    Update update_user.php

--- update_user.php
+++ update_user.php
@@ -1,12 +1,11 @@
 <?php
 require 'db_connection.php';
 
-if (isset($_POST['id'], $_POST['first_name'], $_POST['last_name'],$_POST['password'], $_POST['email'], $_POST['User_Education'], $_POST['User_Experience'], $_POST['User_Skillls'])) {
+if (isset($_POST['id'], $_POST['User_name'],$_POST['password'], $_POST['email'], $_POST['User_Education'], $_POST['User_Experience'], $_POST['User_Skillls'])) {
     
   
     $id = mysqli_real_escape_string($con, $_POST['id']);
-    $first_name = mysqli_real_escape_string($con, $_POST['first_name']);
-    $last_name = mysqli_real_escape_string($con, $_POST['last_name']);
+    $User_name = mysqli_real_escape_string($con, $_POST['User_name']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $User_Education = mysqli_real_escape_string($con, $_POST['User_Education']);
@@ -16,7 +15,7 @@ if (isset($_POST['id'], $_POST['first_name'], $_POST['last_name'],$_POST['passwo
     
     if (is_numeric($id)) {
         
-        $sql_update = "UPDATE users SET id='$id',first_name='$first_name', last_name='$last_name', email='$email',password='$password',
+        $sql_update = "UPDATE users SET id='$id',User_name='$User_name',  email='$email',password='$password',
         User_Education='$User_Education', User_Experience='$User_Experience', User_Skillls='$User_Skillls' WHERE id=$id";
         
         
