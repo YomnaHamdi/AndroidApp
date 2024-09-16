a160954cdf214d262d2a85745dfc96d16e9320e9
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-16 21:20:20 +0300

    Update Insert_CV.php

--- Insert_CV.php
+++ Insert_CV.php
@@ -6,14 +6,16 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 if (
     isset($data['User_id']) &&
+    isset($data['User_name']) &&
+    isset($data['Phone']) &&
     isset($data['Languages']) &&
     isset($data['Skills']) &&
     isset($data['WorkExperience']) &&
     isset($data['Education']) &&
     isset($data['Certifications'])
 ) {
-    $stmt = $con->prepare("INSERT INTO curriculum_vitae (User_id, Created_at, Languages, Skills, WorkExperience, Education, Certifications) VALUES (?, NOW(), ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $data['User_id'], $data['Languages'], $data['Skills'], $data['WorkExperience'], $data['Education'], $data['Certifications']);
+    $stmt = $con->prepare("INSERT INTO curriculum_vitae (User_id, User_name, Phone, Created_at, Languages, Skills, WorkExperience, Education, Certifications) VALUES (?, ?, ?, NOW(), ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssss", $data['User_id'], $data['User_name'], $data['Phone'], $data['Languages'], $data['Skills'], $data['WorkExperience'], $data['Education'], $data['Certifications']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "CV uploaded successfully."));
