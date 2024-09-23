a55abe2a28b444a242788da78a7d6a4fd6839c06
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-23 17:41:05 +0300

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -4,12 +4,12 @@ require 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['id']) && !empty($data['id'])) {
-    $id = mysqli_real_escape_string($con, $data['id']);
+if (isset($data['User_id']) && !empty($data['User_id'])) {
+    $User_id = mysqli_real_escape_string($con, $data['User_id']);
     
-    if (is_numeric($id)) {
-        $stmt = $con->prepare("DELETE FROM users WHERE id = ?");
-        $stmt->bind_param("i", $id);
+    if (is_numeric($User_id)) {
+        $stmt = $con->prepare("DELETE FROM users WHERE User_id = ?");
+        $stmt->bind_param("i", $User_id);
         
         if ($stmt->execute()) {
             echo json_encode(["status" => "success", "message" => "User deleted successfully"]);
