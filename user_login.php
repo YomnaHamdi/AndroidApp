a3ca5c43c6fc2a724c22fa007b83ef8ee2359b2e
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-24 22:07:01 +0300

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -6,10 +6,12 @@ use \Firebase\JWT\JWT;
 
 $data = json_decode(file_get_contents("php://input"), true);
 
+
 if (isset($data['email']) && isset($data['password'])) {
     $email = $data['email'];
     $password = $data['password'];
 
+    
     $stmt = $con->prepare("SELECT User_id, password FROM users WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
@@ -19,20 +21,21 @@ if (isset($data['email']) && isset($data['password'])) {
         $stmt->bind_result($User_id, $hashedPassword);
         $stmt->fetch();
 
+        
         if (password_verify($password, $hashedPassword)) {
-            // إعداد التوكن
+        
             $secret_key = "9%fG8@h7!wQ4$zR2*vX3&bJ1#nL6!mP5"; 
             $expiration_time = time() + (60 * 60); // مدة صلاحية التوكن (ساعة)
             $token = array(
                 "iat" => time(), // تاريخ الإنشاء
                 "exp" => $expiration_time, // تاريخ الانتهاء
                 "data" => array(
-                    "User_id" => $User_id // 
+                    "User_id" => $User_id 
                 )
             );
 
-            // إنشاء التوكن
-            $jwt = JWT::encode($token, $secret_key, 'HS256'); // خوارزمية سريعة فى التشفير وفك التشفير
+            
+            $jwt = JWT::encode($token, $secret_key, 'HS256');
 
             echo json_encode(array(
                 "message" => "Login successful",
@@ -49,4 +52,3 @@ if (isset($data['email']) && isset($data['password'])) {
 }
 
 mysqli_close($con);
-?>
