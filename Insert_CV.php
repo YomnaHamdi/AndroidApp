48a3085563d48223a88a0c594f328470dc8dc016
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-24 22:24:23 +0300

    Update Insert_CV.php

--- Insert_CV.php
+++ Insert_CV.php
@@ -3,7 +3,7 @@
 include_once 'db_connection.php'; 
 require 'vendor/autoload.php'; 
 use \Firebase\JWT\JWT;
-use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
 require 'auth.php'; 
 
 $data = json_decode(file_get_contents("php://input"), true);
