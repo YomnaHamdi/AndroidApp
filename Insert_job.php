72443ddfef4c4f6acabf43ebfeb2a4555fd97423
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-24 17:38:58 +0300

    Update Insert_job.php

--- Insert_job.php
+++ Insert_job.php
@@ -17,13 +17,14 @@ if (
     isset($data['star']) && 
     isset($data['Date']) &&
     isset($data['job_mode']) &&
+    isset($data['industry']) &&
     isset($data['company_name']) 
 ) {
     
-    $stmt = $con->prepare("INSERT INTO jobs ( Company_id, Job_title, Job_description, Employment_type, Job_location, Salary_range, Requirements, Job_type, Posted_at, star,Date,job_mode, company_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?,?,?, ?)");
+    $stmt = $con->prepare("INSERT INTO jobs ( Company_id, Job_title, Job_description, Employment_type, Job_location, Salary_range, Requirements, Job_type, Posted_at, star,Date,job_mode,industry, company_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?,?,?,?, ?)");
     
     
-    $stmt->bind_param("isssssssisss",  $data['Company_id'], $data['Job_title'], $data['Job_description'], $data['Employment_type'], $data['Job_location'], $data['Salary_range'], $data['Requirements'], $data['Job_type'], $data['star'],$data['Date'],$data['job_mode'], $data['company_name']);
+    $stmt->bind_param("isssssssissss",  $data['Company_id'], $data['Job_title'], $data['Job_description'], $data['Employment_type'], $data['Job_location'], $data['Salary_range'], $data['Requirements'], $data['Job_type'], $data['star'],$data['Date'],$data['job_mode'],$data['industry'], $data['company_name']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Job created successfully."));
