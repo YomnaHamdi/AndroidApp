16cde6852b10ff000bcc962208eaea65527c2b93
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-09-26 23:50:47 +0300

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -6,12 +6,10 @@ use \Firebase\JWT\JWT;
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-
 if (isset($data['email']) && isset($data['password'])) {
     $email = $data['email'];
     $password = $data['password'];
 
-    
     $stmt = $con->prepare("SELECT User_id, password FROM users WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
@@ -21,20 +19,17 @@ if (isset($data['email']) && isset($data['password'])) {
         $stmt->bind_result($User_id, $hashedPassword);
         $stmt->fetch();
 
-        
         if (password_verify($password, $hashedPassword)) {
-        
             $secret_key = "9%fG8@h7!wQ4$zR2*vX3&bJ1#nL6!mP5"; 
             $expiration_time = time() + (60 * 60); // مدة صلاحية التوكن (ساعة)
             $token = array(
-                "iat" => time(), // تاريخ الإنشاء
-                "exp" => $expiration_time, // تاريخ الانتهاء
+                "iat" => time(),
+                "exp" => $expiration_time,
                 "data" => array(
                     "User_id" => $User_id 
                 )
             );
 
-            
             $jwt = JWT::encode($token, $secret_key, 'HS256');
 
             echo json_encode(array(
@@ -52,3 +47,4 @@ if (isset($data['email']) && isset($data['password'])) {
 }
 
 mysqli_close($con);
+?>
