e01948cfd12b36936ecaa97a684cce08730c8400
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-10-09 15:54:17 +0300

    Update insert_certificate.php

--- insert_certificate.php
+++ insert_certificate.php
@@ -4,10 +4,12 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (isset($data['certificate_name']) && isset($data['institution']) && isset($data['issue_date']) && isset($data['description'])) {
+
+if (isset($data['User_id']) && isset($data['certificate_name']) && isset($data['institution']) && isset($data['issue_date']) && isset($data['description'])) {
     
-    $stmt = $con->prepare("INSERT INTO certificate (certificate_name, institution,issue_date, description) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $data['certificate_name'], $data['institution'], $data['issue_date'], $data['description']);
+    
+    $stmt = $con->prepare("INSERT INTO certificate (User_id, certificate_name, institution, issue_date, description) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("issss", $data['User_id'], $data['certificate_name'], $data['institution'], $data['issue_date'], $data['description']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Certificate added successfully."));
