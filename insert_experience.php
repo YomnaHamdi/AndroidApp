9cf95c8da77f2fd37a895886ccabc51f9978f055
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-10-09 15:56:18 +0300

    Update insert_experience.php

--- insert_experience.php
+++ insert_experience.php
@@ -4,15 +4,18 @@ include_once 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
+
 if (
+    isset($data['User_id']) &&
     isset($data['company']) &&
     isset($data['title']) && 
     isset($data['start_date']) &&
     isset($data['end_date']) &&
     isset($data['description'])
 ) {
-    $stmt = $con->prepare("INSERT INTO experience (company, title, start_date, end_date, description) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $data['company'], $data['title'], $data['start_date'], $data['end_date'], $data['description']);
+    
+    $stmt = $con->prepare("INSERT INTO experience (User_id, company, title, start_date, end_date, description) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("isssss", $data['User_id'], $data['company'], $data['title'], $data['start_date'], $data['end_date'], $data['description']);
     
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Experience added successfully."));
