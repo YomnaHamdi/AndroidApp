2eb2a02c43156267691b0215f94a38c93050f947
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-10-14 00:52:52 +0300

    Update insert_companies.php

--- insert_companies.php
+++ insert_companies.php
@@ -7,11 +7,13 @@ $Contact_person = $_POST['Contact_person'];
 $location = $_POST['location'];
 $industry = $_POST['industry'];
 
-$sql_insert = "INSERT INTO companies (Company_name, Contact_person, location, industry) 
-VALUE('$Company_name', '$Contact_person', '$location', '$industry')";
+
+$sql_insert = "INSERT INTO companies (Company_name, Contact_person, location, industry) VALUES ('$Company_name', '$Contact_person', '$location', '$industry')";
 
 if (mysqli_query($con, $sql_insert)) {
-    echo json_encode(["status" => "success", "message" => "Company added successfully"]);
+    
+    $Company_id = mysqli_insert_id($con);
+    echo json_encode(["status" => "success", "message" => "Company added successfully", "Company_id" => $Company_id]);
 } else {
     echo json_encode(["status" => "error", "message" => "Error adding company: " . mysqli_error($con)]);
 }
