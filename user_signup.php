d92c931bdcc0d283eb88620c044c8ca5451a9257
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-10-22 20:55:39 +0300

    Update user_signup.php

--- user_signup.php
+++ user_signup.php
@@ -4,7 +4,6 @@ use Firebase\JWT\JWT;
 
 include 'db_connection.php'; 
 
-
 $secretKey = "9%fG8@h7!wQ4$zR2*vX3&bJ1#nL6!mP5"; 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -19,15 +18,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $About = $data->About ?? null;
     $password = $data->password ?? null;
 
-    
+   
     if (!$User_name || !$Gender || !$Age || !$Phone || !$Location || !$About || !$password) {
         echo json_encode(["error" => "All fields are required."]);
         http_response_code(400);
         exit();
     }
+
+ 
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    
+   
     $sql_check = "SELECT * FROM users WHERE User_name = ?";
     $stmt_check = $con->prepare($sql_check);
     $stmt_check->bind_param("s", $User_name);
@@ -40,25 +41,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         exit();
     }
 
-
-    $sql = "INSERT INTO users (User_name, password, Gender, Age, Phone, Location, About,created_at) 
-            VALUES (?, ?, ?, ?, ?, ?, ?,NOW())";
+   
+    $sql = "INSERT INTO users (User_name, password, Gender, Age, Phone, Location, About, created_at) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
     $stmt = $con->prepare($sql);
-    $stmt->bind_param("ssssssss", $User_name, $hashedPassword, $Gender, $Age, $Phone, $Location, $About);
+    $stmt->bind_param("sssssss", $User_name, $hashedPassword, $Gender, $Age, $Phone, $Location, $About);
 
     if ($stmt->execute()) {
-    
         $user_id = $stmt->insert_id;
 
-        
+       
         $payload = [
             'iat' => time(),
             'exp' => time() + (60 * 60), 
             'user_id' => $user_id,
-            'username' => $username
+            'username' => $User_name
         ];
 
-        
         $jwt = JWT::encode($payload, $secretKey, 'HS256');
 
         
