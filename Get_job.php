d100f7c70f4671cd1f70663fda8e306241a5012f
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-10-24 17:58:23 +0300

    Update Get_job.php

--- Get_job.php
+++ Get_job.php
@@ -7,13 +7,14 @@ include 'db_connection.php';
 $secretKey = "9%fG8@h7!wQ4\$zR2*vX3&bJ1#nL6!mP5";
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    //الحصول على قيمة البحث 
+    // الحصول على قيمة البحث 
     $searchTerm = $_GET['search'] ?? '';
 
-    // استخدام SQL LIKE للبحث عن الوظائف
-    $sql = "SELECT job_title, job_description, Company_name, Company_location, salary, created_at, job_id 
-            FROM job_posts 
-            WHERE job_title LIKE ? OR Company_name LIKE ?";
+    // استخدام SQL LIKE للبحث عن الوظائف مع JOIN مع جدول companies
+    $sql = "SELECT j.job_title, j.job_description, c.Company_name, c.Company_location, j.salary, j.created_at, j.job_id 
+            FROM jobs j 
+            JOIN companies c ON j.Company_id = c.Company_id 
+            WHERE j.job_title LIKE ? OR c.Company_name LIKE ?";
 
     $stmt = $con->prepare($sql);
     $searchWildcard = "%" . $searchTerm . "%";
@@ -21,13 +22,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $stmt->execute();
     $result = $stmt->get_result();
 
-    $job_posts = [];
+    $jobs = [];
     while ($row = $result->fetch_assoc()) {
         $jobs[] = $row;
     }
 
     echo json_encode($jobs);
-
 } else {
     echo json_encode(["error" => "Method not allowed"]);
 }
