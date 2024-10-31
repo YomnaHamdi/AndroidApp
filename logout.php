9b36b7316149f7872be92a140c085429f128c54b
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-11-01 00:45:26 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,11 @@
 <?php
 header('Content-Type: application/json');
-ob_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // إرسال رسالة النجاح بصيغة JSON فقط
     echo json_encode(["message" => "Logout successful"]);
+    exit; // إنهاء التنفيذ هنا
 } else {
     echo json_encode(["error" => "Method not allowed"]);
+    exit;
 }
-
-ob_end_clean();
