ac78f84f39b7da5c95f355f04b702546f12a50e0
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-11-03 00:50:09 +0200

    Update auth_middleware.php

--- auth_middleware.php
+++ auth_middleware.php
@@ -10,7 +10,7 @@ if (isset($headers['Authorization'])) {
     $token = str_replace('Bearer ', '', $headers['Authorization']);
     
     // تحقق إذا كان التوكن موجودًا في القائمة السوداء
-    $stmt = $conn->prepare("SELECT COUNT(*) FROM token_blacklist WHERE token = ? AND expiry_date > NOW()");
+    $stmt = $con->prepare("SELECT COUNT(*) FROM token_blacklist WHERE token = ? AND expiry_date > NOW()");
     $stmt->bind_param("s", $token);
     $stmt->execute();
     $stmt->bind_result($count);
