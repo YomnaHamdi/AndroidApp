8e177bab3e8107863af35817b2fd31f24604af11
YomnaHamdi <141845620+YomnaHamdi@users.noreply.github.com>
2024-11-06 00:36:03 +0200

    Update Get_Job_Notifications.php

--- Get_Job_Notifications.php
+++ Get_Job_Notifications.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $sql = "SELECT j.job_title, u.User_id as User_id, u.User_name, n.Sent_at 
                 FROM job_notifications n
                 JOIN users u ON n.User_id = u.id
-                JOIN job_posts j ON n.job_id = j.id
+                JOIN job_posts j ON n.job_id = j.job_id
                 WHERE j.Company_id = ?
                 ORDER BY j.job_title, n.Sent_at DESC";
         $stmt = $con->prepare($sql);
